06bfb957202c95def8569b1298f156870c9c3766
japple <hantao@boaocloud.com>
2022-02-10 10:47:24 +0800

    space支持-

--- playground/src/views/Flex.vue
+++ playground/src/views/Flex.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="row flex-0-2-auto flexWrap-wrap flexShrink-3">
+  <div class="column flex-0-2-auto flexWrap-wrap flexShrink-3 spaceY-20">
     <div class="square100 bgBlue500"></div>
     <div class="square150 bgGreen500"></div>
   </div>
--- preset-fower/src/rules/spacings.js
+++ preset-fower/src/rules/spacings.js
@@ -1,29 +1,67 @@
+const selector = '>*:not(:last-child)'
+
 const parse = (k, config) => {
-  if (k.startsWith('spacex')) {
-    const size = Number.parseInt(k.replace('spacex', ''))
+  if (k.startsWith(`spacex${config.vs}`)) {
+    // spaceX-10 横向间距10
+    const [_, val] = k.split(config.vs)
+    const size = Number.parseInt(val)
     if (Number.isNaN(size)) {
       return
     }
     return {
-      selector: '>*:not(:last-child)',
-      'margin-right': `${size * config.sizeUnit}${config.unit}`
+      selector,
+      'margin-right': `${size}${config.unit}`
+    }
+  } else if (k.startsWith(`spacex`)) {
+    // spaceX10 横向间距10 * 系数
+    size = Number.parseInt(k.replace('spacex', ''))
+    if (!Number.isNaN(size)) {
+      return {
+        selector,
+        'margin-right': `${size * config.sizeUnit}${config.unit}`
+      }
+    }
+  } else if (k.startsWith(`spacey${config.vs}`)) {
+    // spaceY-10 纵向间距10
+    const [_, val] = k.split(config.vs)
+    const size = Number.parseInt(val)
+    if (Number.isNaN(size)) {
+      return
+    }
+    return {
+      selector,
+      'margin-bottom': `${size}${config.unit}`
     }
   } else if (k.startsWith('spacey')) {
+    // spaceY10 纵向间距 10 * 系数
     const size = Number.parseInt(k.replace('spacey', ''))
     if (Number.isNaN(size)) {
       return
     }
     return {
-      selector: '>*:not(:last-child)',
+      selector,
       'margin-bottom': `${size * config.sizeUnit}${config.unit}`
     }
+  } else if (k.startsWith(`space${config.vs}`)) {
+    // space10 间距 10
+    const [_, val] = k.split(config.vs)
+    const size = Number.parseInt(val)
+    if (Number.isNaN(size)) {
+      return
+    }
+    return {
+      selector,
+      'margin-right': `${size}${config.unit}`,
+      'margin-bottom': `${size}${config.unit}`
+    }
   } else if (k.startsWith('space')) {
+    // space10 间距 10 * 系数
     const size = Number.parseInt(k.replace('space', ''))
     if (Number.isNaN(size)) {
       return
     }
     return {
-      selector: '>*:not(:last-child)',
+      selector,
       'margin-right': `${size * config.sizeUnit}${config.unit}`,
       'margin-bottom': `${size* config.sizeUnit}${config.unit}`
     }
