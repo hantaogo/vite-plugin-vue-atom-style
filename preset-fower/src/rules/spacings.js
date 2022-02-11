e3994be7cc459e8ddf1584e6db74a63d8fb79200
japple <hantao@boaocloud.com>
2022-02-11 19:38:20 +0800

    spacing

--- preset-fower/src/rules/spacings.js
+++ preset-fower/src/rules/spacings.js
@@ -1,78 +1,43 @@
+import { parseSize } from '../utils'
+
 const selector = '>*:not(:last-child)'
 
+const namesMap = {
+  space: ['margin-right', 'margin-bottom'],
+  spacex: ['margin-right'],
+  spacey: ['margin-bottom'],
+}
+
+const regex = /^(space[xy])(\-?)(.+)$/i
+
 const parse = (k, config) => {
-  if (k.startsWith(`spacex-`)) {
-    // spaceX-10 横向间距10
-    const [_, val] = k.split('-')
-    const size = Number.parseInt(val)
-    if (Number.isNaN(size)) {
-      return
-    }
-    return {
-      selector,
-      'margin-right': `${size}${config.unit}`
-    }
-  } else if (k.startsWith(`spacex`)) {
-    // spaceX10 横向间距10 * 系数
-    size = Number.parseInt(k.replace('spacex', ''))
-    if (!Number.isNaN(size)) {
-      return {
-        selector,
-        'margin-right': `${size * config.unitSize}${config.unit}`
+  const result = k.match(regex)
+  if (Array.isArray(result) && result.length >= 4) {
+    const [_, key, sep, value] = result
+    if (namesMap[key]) {
+      const size = parseSize(value, config.unit, sep ? 1 : config.unitSize)
+      if (size) {
+        return { names: namesMap[key], size }
       }
-    }
-  } else if (k.startsWith(`spacey-`)) {
-    // spaceY-10 纵向间距10
-    const [_, val] = k.split('-')
-    const size = Number.parseInt(val)
-    if (Number.isNaN(size)) {
-      return
-    }
-    return {
-      selector,
-      'margin-bottom': `${size}${config.unit}`
-    }
-  } else if (k.startsWith('spacey')) {
-    // spaceY10 纵向间距 10 * 系数
-    const size = Number.parseInt(k.replace('spacey', ''))
-    if (Number.isNaN(size)) {
-      return
-    }
-    return {
-      selector,
-      'margin-bottom': `${size * config.unitSize}${config.unit}`
-    }
-  } else if (k.startsWith(`space-`)) {
-    // space10 间距 10
-    const [_, val] = k.split('-')
-    const size = Number.parseInt(val)
-    if (Number.isNaN(size)) {
-      return
-    }
-    return {
-      selector,
-      'margin-right': `${size}${config.unit}`,
-      'margin-bottom': `${size}${config.unit}`
-    }
-  } else if (k.startsWith('space')) {
-    // space10 间距 10 * 系数
-    const size = Number.parseInt(k.replace('space', ''))
-    if (Number.isNaN(size)) {
-      return
-    }
-    return {
-      selector,
-      'margin-right': `${size * config.unitSize}${config.unit}`,
-      'margin-bottom': `${size* config.unitSize}${config.unit}`
     }
   }
 }
 
 export default {
   match: (k, config) => {
-    return !!parse(k, config)
+    return regex.test(k)
   },
   translate: (k, config) => {
-    return parse(k, config)
+    const result = parse(k, config)
+    if (result) {
+      const { names, size } = result
+      const style = {
+        selector,
+      }
+      for (const name of names) {
+        style[name] = size
+      }
+      return style
+    }
   }
 }
