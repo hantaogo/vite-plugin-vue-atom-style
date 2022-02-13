d91f9bda72cd95df4b6fc055f734c7efa0027c8d
japple <hantao@boaocloud.com>
2022-02-13 23:42:23 +0800

    demo按字母排序

--- playground/src/views/Base.vue
+++ playground/src/views/Base.vue
@@ -5,7 +5,7 @@ import { useRouter } from 'vue-router'
 
 const router = useRouter()
 
-const items = ref(routes.filter(t => t.path !== '/'))
+const items = ref(routes.filter(t => t.path !== '/').map(t => t.path.replace('/', '')).sort())
 
 const go = path => {
   router.push(path)
@@ -14,6 +14,6 @@ const go = path => {
 
 <template>
   <div class="grid gridTemplateColumns-3 gap4">
-    <div v-for="(item, index) of items" :key="index" class="button w-100p" @click="go(item.path)">{{ item.path.replace('/', '') }}</div>
+    <div v-for="(item, index) of items" :key="index" class="button w-100p" @click="go(item.path)">{{ item }}</div>
   </div>
 </template>
