ddcb904db91e20a36e221e5cdb5cdbfcebe39ab6
japple <hantao@boaocloud.com>
2022-02-14 01:13:17 +0800

    修复演示菜单

--- playground/src/views/Base.vue
+++ playground/src/views/Base.vue
@@ -14,6 +14,6 @@ const go = path => {
 
 <template>
   <div class="grid gridTemplateColumns-3 gap4">
-    <div v-for="(item, index) of items" :key="index" class="button w-100p" @click="go(item.path)">{{ item }}</div>
+    <div v-for="(item, index) of items" :key="index" class="button w-100p" @click="go(item)">{{ item }}</div>
   </div>
 </template>
