f95050084e10424a94da6cf86482bbb9919590c5
japple <hantao@boaocloud.com>
2022-05-30 11:44:24 +0800

    热更新时，也进行跳过检查

--- core/src/index.js
+++ core/src/index.js
@@ -4,26 +4,31 @@ import { generate } from './generate'
 export default (args = {}) => {
   const options = mergeOptions(args)
   const fileRegex = options.fileRegex || /\.(vue)$/
-
+  const check = (filePath) => {
+    if (!fileRegex.test(filePath)) {
+      return false
+    }
+    // 忽略有标记的文件
+    if (options.ignoreFlag && code.startsWith(options.ignoreFlag)) {
+      return false
+    }
+    // 只使用有标记的文件
+    if (options.flag && !code.startsWith(options.flag)) {
+      return false
+    }
+    return true
+  }
   return {
     name: 'vite-plugin-vue-atom-style',
-    transform(code, id) {
-      if (!fileRegex.test(id)) {
-        return
-      }
-      // 忽略有标记的文件
-      if (options.ignoreFlag && code.startsWith(options.ignoreFlag)) {
-        return
-      }
-      // 只使用有标记的文件
-      if (options.flag && !code.startsWith(options.flag)) {
+    transform(code, filePath) {
+      if (!check(filePath)) {
         return
       }
       return transformSFC(code, options)
     },
     handleHotUpdate(ctx) {
       const read = ctx.read
-      if (fileRegex.test(ctx.file)) {
+      if (check(ctx.file)) {
         ctx.read = async () => {
           const code = await read()
           return transformSFC(code, options) || code
