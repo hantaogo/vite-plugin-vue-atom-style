86c2136af1056cb173176652a21f4b6fa2efc0ac
japple <hantao@boaocloud.com>
2022-07-06 10:45:22 +0800

    修复热更新bug

--- core/src/index.js
+++ core/src/index.js
@@ -4,35 +4,38 @@ import { generate } from './generate'
 export default (args = {}) => {
   const options = mergeOptions(args)
   const fileRegex = options.fileRegex || /\.(vue)$/
-  const check = (filePath) => {
-    if (!fileRegex.test(filePath)) {
-      return false
-    }
-    // 忽略有标记的文件
-    if (options.ignoreFlag && code.startsWith(options.ignoreFlag)) {
-      return false
-    }
-    // 只使用有标记的文件
-    if (options.flag && !code.startsWith(options.flag)) {
-      return false
-    }
-    return true
-  }
   return {
     name: 'vite-plugin-vue-atom-style',
     transform(code, filePath) {
-      if (!check(filePath)) {
+      if (!fileRegex.test(filePath)) {
+        return
+      }
+      // 忽略有标记的文件
+      if (options.ignoreFlag && code.startsWith(options.ignoreFlag)) {
+        return
+      }
+      // 只使用有标记的文件
+      if (options.flag && !code.startsWith(options.flag)) {
         return
       }
       return transformSFC(code, options)
     },
     handleHotUpdate(ctx) {
+      if (!fileRegex.test(ctx.file)) {
+        return
+      }
       const read = ctx.read
-      if (check(ctx.file)) {
-        ctx.read = async () => {
-          const code = await read()
-          return transformSFC(code, options) || code
+      ctx.read = async () => {
+        const code = await read()
+        // 忽略有标记的文件
+        if (options.ignoreFlag && code.startsWith(options.ignoreFlag)) {
+          return
         }
+        // 只使用有标记的文件
+        if (options.flag && !code.startsWith(options.flag)) {
+          return
+        }
+        return transformSFC(code, options) || code
       }
     },
   }
